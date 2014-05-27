7a39540c9855a77bfc7b4542ca86cb674972658b
jsguy <mikkel@pointful.com>
2014-05-27 17:24:56 +1000

    Fixed example

--- app/index.js
+++ app/index.js
@@ -2,19 +2,18 @@ var getsrc = require('./getsrc.js');
 
 //	Test lookup
 var lookup = {
-	//url: "http://192.168.7.207/sourcemaptest/jquery-1.11.0.min.js",
-	url: "http://192.168.7.207/sourcemaptest/jquery-1.11.0.min.js",
-	//url: "http://nothere/sourcemaptest/jquery-1.11.0.min.js",
+	url: "http://jsguy.com/labs/srcify/jquery-1.11.0.min.js",
 	line: 3,
 	column: 26
 };
 
 //	Grab the src
+console.log('Grabbing file:', lookup.url);
 getsrc(lookup, function(error, code){
 	if(code && ! error) {
+		console.log('--- Result ---');
 		console.log(code);
 	} else {
-		//	
 		console.log('Something went wrong whilst trying to fetch the code.', error);
 	}
 });
